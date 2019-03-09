5c97de01dbc98b8f3a7e737a790726517e976e26
Vlad Guryev <teylor-world@yandex.ru>
2019-03-10 02:22:11 +0300

    working but not optimazed version of text Editor; tests are passed

--- main.cpp
+++ main.cpp
@@ -9,19 +9,138 @@ int main()
 {
   TestRunner tr;
   LOG_DURATION("Total tests duration");
-//  RUN_TEST(tr, TestEditing);
-//  RUN_TEST(tr, TestReverse);
-//  RUN_TEST(tr, TestNoText);
-//  RUN_TEST(tr, TestEmptyBuffer);
+  RUN_TEST(tr, TestEditing);
+  RUN_TEST(tr, TestReverse);
+  RUN_TEST(tr, TestNoText);
+  RUN_TEST(tr, TestEmptyBuffer);
 
-  string* t = new string("AAAAAAAAAaaaa");
-  string_view sv(*t);
-  delete t;
-  for (int i = 0; i < 10000; i++) {
-    string* p = new string("123456789");
-    p->at(1);
-  }
-  cout << sv << endl;
+//  string* t = new string("AAAAAAAAAaaaa");
+//  string_view sv(*t);
+//  delete t;
+//  for (int i = 0; i < 10000; i++) {
+//    string* p = new string("123456789");
+//    p->at(1);
+//  }
+//  cout << sv << endl;
+
+
+//  auto debugInfo = [](const Editor& editor){
+//    cout << "text = {" <<editor.GetText() << "}" << endl;
+//    cout << "position = " << editor.GetCursor().getPosition() << endl;
+//    cout << "buffer = {"  << editor.GetBuffer() << "}" << endl;
+//  };
+  //--------------------------
+
+//  Editor editor;
+//  debugInfo(editor);
+//  TypeText(editor, "a");
+//  debugInfo(editor);
+//  TypeText(editor, "a");
+//  debugInfo(editor);
+
+//  Editor editor2;
+//  TypeText(editor2, "ab");
+//  debugInfo(editor2);
+//  editor2.Insert('c');
+//  debugInfo(editor2);
+
+//  Editor editor3;
+//  debugInfo(editor3);
+//  TypeText(editor3, "bc");
+//  debugInfo(editor3);
+//  editor3.Left();
+//  editor3.Left();
+//  debugInfo(editor3);
+//  editor3.Insert('a');
+//  debugInfo(editor3);
+
+//  Editor editor4;
+//  debugInfo(editor4);
+//  TypeText(editor4, "world");
+//  debugInfo(editor4);
+//  for (int i = 0; i < 5; ++i) {
+//    editor4.Left();
+//  }
+//  debugInfo(editor4);
+//  TypeText(editor4, "hello_");
+//  debugInfo(editor4);
+
+//  Editor editor5;
+//  debugInfo(editor5);
+//  TypeText(editor5, "hello_");
+//  debugInfo(editor5);
+//  TypeText(editor5, "world");
+//  debugInfo(editor5);
+
+//  Editor editor6;
+//  TypeText(editor6, "12378");
+//  debugInfo(editor6);
+//  editor6.Left();
+//  editor6.Left();
+//  debugInfo(editor6);
+//  TypeText(editor6, "456");
+//  debugInfo(editor6);
+
+/*
+  Editor editor;
+    const string text = "hello, world";
+    for (char c : text) {
+      editor.Insert(c);
+    }
+    // Текущее состояние редактора: `hello, world|`
+    debugInfo(editor);
+    for (size_t i = 0; i < text.size(); ++i) {
+      editor.Left();
+    }
+     debugInfo(editor);
+    // Текущее состояние редактора: `|hello, world`
+
+
+     editor.Cut(7);
+//    // Текущее состояние редактора: `|world`
+      debugInfo(editor);
+//    // в буфере обмена находится текст `hello, `
+    for (size_t i = 0; i < 5; ++i) {
+      editor.Right();
+    }
+    debugInfo(editor);
+//    // Текущее состояние редактора: `world|`
+    editor.Insert(',');
+    editor.Insert(' ');
+    debugInfo(editor);
+//    // Текущее состояние редактора: `world, |`
+    editor.Paste();
+    debugInfo(editor);
+//    // Текущее состояние редактора: `world, hello, |`
+    editor.Left();
+    editor.Left();
+    debugInfo(editor);
+//    //Текущее состояние редактора: `world, hello|, `
+    editor.Cut(3); // Будут вырезаны 2 символа
+    debugInfo(editor);
+//    // Текущее состояние редактора: `world, hello|`
+    cout << editor.GetText();
+*/
+
+//  Editor editor7;
+//  TypeText(editor7, "abcd");
+//  debugInfo(editor7);
+//  for (int i = 0; i < 100; ++i) {
+//    editor7.Left();
+//  }
+//  debugInfo(editor7);
+//  for (int i = 0; i < 100; ++i) {
+//    editor7.Right();
+//  }
+//  debugInfo(editor7);
+
+//  Editor editor8;
+//  TypeText(editor8, "abcdef");
+//  debugInfo(editor8);
+//  editor8.Left(); editor8.Left(); editor8.Left(); editor8.Left();
+//  debugInfo(editor8);
+//  editor8.Cut(3);
+//  debugInfo(editor8);
 
   return 0;
 }
--- texteditor.h
+++ texteditor.h
@@ -1,24 +1,43 @@
 #pragma once
 #include <iostream>
+#include <string>
+//#include <experimental/string_view>
+
 using namespace std;
+//using namespace std::experimental;
 
 class Editor {
 private:
   class Position{
   public:
-    Position() = default;
-    Position(size_t string_size): text_size(string_size){
+    Position() {
 
     }
+
     Position& operator++(){
       if(text_size > 0 && pos < text_size)
         this->pos++;
       return *this;
     }
     Position& operator--(){
-      if(text_size > 0 && pos > 0)
+      if(pos > 0)
         this->pos--;
       return *this;
+    }
+    void Insert(){
+      pos++;
+      text_size++;
+    }
+    size_t getPosition() const{
+      return pos;
+    }
+    void nextPosition(size_t next){
+      size_t newPosition = pos + next;
+      if(newPosition <= text_size)
+        pos = newPosition;
+      else {
+        pos = text_size;
+      }
     }
 
   private:
@@ -26,37 +45,45 @@ private:
     size_t text_size = 0;
   };
 
-
-  Position cursor; //string_view or iterator or pointer
+  Position* cursor;
   string text;
-  //??? buffer; //for copy paste operations
+  string buffer; //for copy paste operations
+
 public:
   Editor(){
-    //empty text;
-    //empty buffer;
+    cursor = new Position();
   }
   void Left(){
-    --cursor;
+    --(*cursor);
   }
   void Right(){
-    ++cursor;
+    ++(*cursor);
   }
   void Insert(char token){
-    //text.push_back(token);
+    text.insert(cursor->getPosition(), new char(token));
+    cursor->Insert();
   }
-  //
   void Cut(size_t tokens = 1){
-
+   buffer = text.substr(cursor->getPosition(), tokens);
+   text.erase(cursor->getPosition(), tokens);
   }
   void Copy(size_t tokens = 1){
-
+    buffer = text.substr(cursor->getPosition(), tokens);
   }
   void Paste(){
-
+    text.insert(cursor->getPosition(), buffer);
+    cursor->nextPosition(buffer.size());
   }
   string GetText() const{
     return text;
   }
+  //debug methods
+//  const Position& GetCursor() const{
+//    return *cursor;
+//  }
+//  const string& GetBuffer() const {
+//    return  buffer;
+//  }
 };
 
 void TypeText(Editor& editor, const string& text) {
